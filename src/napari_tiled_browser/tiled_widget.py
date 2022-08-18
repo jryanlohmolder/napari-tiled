40ceb46fa6c1cb6d7bbe2103d6bc9187aa753db1
Abigail Giles <agiles@bnl.gov>
2022-08-18 18:24:43 -0400

    Root node table + cleanup and layout updates

--- src/napari_tiled_browser/tiled_widget.py
+++ src/napari_tiled_browser/tiled_widget.py
@@ -8,7 +8,17 @@ Replace code below according to your needs.
 """
 
 from napari.utils.notifications import show_info
-from qtpy.QtWidgets import QLabel, QPushButton, QTextEdit, QVBoxLayout, QWidget
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import (
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QSplitter,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+)
 from tiled.client import from_uri
 
 
@@ -22,29 +32,55 @@ class TiledBrowser(QWidget):
         self.viewer = napari_viewer
         self.catalog = None
 
-        self.url_entry = QTextEdit()
+        # self.url_entry = QTextEdit()
+        self.url_entry = QLineEdit()
+        self.url_entry.setPlaceholderText("Enter a url")
         self.connect_button = QPushButton("Connect")
-        self.test_label = QLabel("No url connected")
+        self.connection_label = QLabel("No url connected")
+
+        self.connection_widget = QWidget()
+        connection_layout = QVBoxLayout()
+        connection_layout.addWidget(self.url_entry)
+        connection_layout.addWidget(self.connect_button)
+        connection_layout.addWidget(self.connection_label)
+        connection_layout.addStretch()
+        self.connection_widget.setLayout(connection_layout)
+
+        self.catalog_table = QTableWidget(0, 1)
+        self.catalog_table.setHorizontalHeaderLabels(["ID"])
+        self.catalog_table.setVisible(False)
+
+        self.splitter = QSplitter(self)
+        self.splitter.setOrientation(Qt.Orientation.Vertical)
+
+        self.splitter.addWidget(self.connection_widget)
+        self.splitter.addWidget(self.catalog_table)
+
+        self.splitter.setStretchFactor(1, 2)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.url_entry)
-        layout.addWidget(self.connect_button)
-        layout.addWidget(self.test_label)
+        layout.addWidget(self.splitter)
         self.setLayout(layout)
 
         self.connect_button.clicked.connect(self._on_connect_clicked)
 
     def _on_connect_clicked(self):
-        url = self.url_entry.toPlainText()
+        url = self.url_entry.displayText()
         if not url:
             show_info("Please specify a url.")
             return
-        print(f"{url = }")
         try:
             self.catalog = from_uri(url)
         except Exception:
-            self.test_label.setText("No url connected")
             show_info("Could not connect. Please check the url.")
             return
-        print(self.catalog)
-        self.test_label.setText(f"Connected to {url}")
+        self.connection_label.setText(f"Connected to {url}")
+        self.catalog_table.setVisible(True)
+        self._populate_table(self.catalog)
+
+    def _populate_table(self, catalog):
+        for node in catalog:
+            item = QTableWidgetItem(node)
+            last_row_position = self.catalog_table.rowCount()
+            self.catalog_table.insertRow(last_row_position)
+            self.catalog_table.setItem(last_row_position, 0, item)
